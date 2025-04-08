d099a289ccaac08cec6f0c33e377a85146ffc4af
Mohammed Daoudi <M.Iduoad@gmail.com>
2025-04-08 02:35:50 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 
-pwd
-ls -al 
-echo "I hate github container actions xD"
-cd /app
-npm start
+# pwd
+# ls -al 
+# echo "I hate github container actions xD"
+# cd /app
+npm start --prefix /app
